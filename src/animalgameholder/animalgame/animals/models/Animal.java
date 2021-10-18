cea1ff978ced9561171beaf10cb8ee487fff3346
Benjis99 <91055304+Benjis99@users.noreply.github.com>
2021-10-18 14:12:02 +0200

    Fixed the red problems in animal

--- src/animalgameholder/animalgame/animals/models/Animal.java
+++ src/animalgameholder/animalgame/animals/models/Animal.java
@@ -12,106 +12,96 @@ import animalgameholder.animalgame.foods.models.Food;
  *
  * @author Lukas L, Isabella S, Benjamin E, Carl M
  */
-public abstract class Animal  {
+public abstract class Animal {
     /**
      * This is our field variables
      */
-        //Change to private or protected
-        public int health = 100;
-        public String name;
-        public MaleFemale gender;
-        public int age = 0;
+    //Change to private or protected
+    protected int health = 100;
+    protected String name;
+    protected MaleFemale gender;
+    protected int age = 0;
 
 
-
-
-    public Animal(String name, MaleFemale mf) {
+    public Animal(String name, MaleFemale gender) {
         this.name = name;
-        this.gender = mf;
+        this.gender = gender;
     }
 
-        public boolean eatTrue(Food food){
-            return true;
-         }
+    public boolean eatTrue(Food food) {
+        return true;
+    }
 
     /**
      * eatFood class will check with the animal if it can eat the food we are giving to it.
      * If the animal is at 100% health then it won't be able to eat anything.
+     *
      * @param food this is in the method, so we can get information from the Food class
      */
-    public void eatFood(Food food){ //Method that will check if the animal can eat the food or not
-                                        //If the animal is at 100 health, then it will not be able to eat
+    public void eatFood(Food food) { //Method that will check if the animal can eat the food or not
+        //If the animal is at 100 health, then it will not be able to eat
         if (eatTrue(food)) {
-            if (this.health >= 100){
+            if (this.health >= 100) {
                 this.health = 100;
                 System.out.println("Animal is at full health");
             }
-            if (this.health < 100){
+            if (this.health < 100) {
                 this.health = this.health + (int) (this.health * 0.10);
 
-                    if (this.health > 100){
-                        this.health = 100;
-                        System.out.println(getName() + "animal is at full health " + getHealth() + "%");
-                    }
+                if (this.health > 100) {
+                    this.health = 100;
+                    System.out.println(getName() + "animal is at full health " + getHealth() + "%");
                 }
             }
         }
+    }
 
+        /*
         public int currentPriceAnimal(){
         double currentPrice = ((this.health / 100.0) * this.startPrice);
         currentPrice = currentPrice - (this.age * 2);
         return (int) currentPrice;
         }
 
-        public boolean canEat(Food food){
+         */
+
+    public boolean canEat(Food food) {
         return true;
-        }
+    }
 
-        public MaleFemale getGender() {
-            return gender;
-        }
-
-
-        public void setGender(String gender) {
-            this.gender = MaleFemale.valueOf(gender.toUpperCase());
-        }
-
-        public String getAnimalBreed() {
-            return animalBreed;
-        }
-
-        public int getStartPrice() {
-            return startPrice;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public int getHealth() {
-            return health;
-        }
-
-        public void setHealth(int health) {
-            this.health = health;
-        }
+    public MaleFemale getGender() {
+        return gender;
+    }
 
 
-        public int getAge() {
+    public void setGender(String gender) {
+        this.gender = MaleFemale.valueOf(gender.toUpperCase());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+
+    public int getAge() {
         return age;
-        }
+    }
 
-        public void setAge(int age) {
+    public void setAge(int age) {
         this.age = this.age + age;
-        }
+    }
 
-        public int getMaxAge() {
-        return maxAge;
-        }
-
-    //Animal Class
-}
+        //Animal Class
+    }
