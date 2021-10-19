5886b18f8bd4ce6f2bd3d9f10c80309152438331
Benjis99 <91055304+Benjis99@users.noreply.github.com>
2021-10-19 16:29:18 +0200

    Delete src/animalgameholder/animalgame/enums directory

--- src/animalgameholder/animalgame/enums/MaleFemale.java
+++
@@ -1,9 +0,0 @@
-package animalgameholder.animalgame.enums;
-
-public enum MaleFemale{
-    FEMALE, MALE;
-
-    public static MaleFemale getRandomSelectGender(){  //Random select for male or female
-        return values()[(int) (Math.random() * values().length)];
-    }
-}
