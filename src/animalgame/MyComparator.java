3637ee41336df9ed4808bdf9715cb9af33949dc1
LuLukas97 <lukas.lundblad@live.se>
2021-11-15 09:16:57 +0100

    Removed unneccesary if else statement in My Comparator

--- src/animalgame/MyComparator.java
+++ src/animalgame/MyComparator.java
@@ -9,8 +9,6 @@ public class MyComparator implements Comparator<Player> {
     public int compare(Player o1, Player o2) {
         if (o1.getCoins() > o2.getCoins()) {
             return -1;
-        } else if (o1.getCoins() < o2.getCoins()) {
-            return 1;
         }
         return 0;
     }
